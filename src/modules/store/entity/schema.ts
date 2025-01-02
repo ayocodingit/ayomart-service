b6c7f5a041c499d5840c4c93bf8601df9fa359ff
ayocodingit <ayocodingit@gmail.com>
2025-01-02 10:38:00 +0700

    add module store

--- src/modules/store/entity/schema.ts
+++ src/modules/store/entity/schema.ts
@@ -3,7 +3,7 @@ import { Store } from './interface'
 
 // define for schema validate
 export const StoreSchema = Joi.object<Store>({
-    address: Joi.string().required(),
+    address: Joi.string().allow('').required(),
     name: Joi.string().required(),
     tax: Joi.number().required(),
     isTaxBorneCustomer: Joi.boolean().required(),
